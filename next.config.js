7511a6301eb8ca52069ddfee9d9f5333abd6557e
JeffreyChix <chiedoziejeffrey@gmail.com>
2023-04-04 03:01:49 +0100

    adjustments

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["the-headless-blog.local"],
+    domains: ["the-headless-blog.local", "api-headless-blog.000webhostapp.com"],
   },
 };
 
